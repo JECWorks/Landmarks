9df9a9be7641907d4a39cb2faba7f3a883863882
JECWorks <36215363+JECWorks@users.noreply.github.com>
2024-06-21 21:24:39 -0400

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Jason Cox on 6/21/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
