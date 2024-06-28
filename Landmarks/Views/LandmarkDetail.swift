299129f508fa10137fdabed2f07c60f4d2f59298
JECWorks <36215363+JECWorks@users.noreply.github.com>
2024-06-27 22:15:38 -0400

    LandmarkDetail and navigationlink between pages

---
+++ Landmarks/Views/LandmarkDetail.swift
@@ -0,0 +1,50 @@
+//
+//  LandmarkDetail.swift
+//  Landmarks
+//
+//  Created by Jason Cox on 6/27/24.
+//
+
+import SwiftUI
+
+
+struct LandmarkDetail: View {
+    var body: some View {
+        VStack {
+            MapView()
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                
+                Text("Turtle Rock")
+                    .font(.title)
+                    .foregroundColor(.black)
+                HStack {
+                    Text("Joshua Tree National Park")
+                    Spacer()
+                    Text("California")
+                    
+                }
+                .font(.subheadline)
+                .foregroundStyle(.secondary)
+                
+                Divider()
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here")
+                
+            }
+            .padding()
+            Spacer()
+        }
+    }
+}
+
+
+#Preview {
+    LandmarkDetail()
+}
