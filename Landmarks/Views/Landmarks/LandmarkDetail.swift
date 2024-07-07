5e15ac1e351c7089aa5985dbe2c21360323d91d8
JECWorks <36215363+JECWorks@users.noreply.github.com>
2024-07-06 22:53:51 -0400

    .environment (:0) will pass data down the hierarchy

--- Landmarks/Views/Landmarks/LandmarkDetail.swift
+++ Landmarks/Views/Landmarks/LandmarkDetail.swift
@@ -9,9 +9,16 @@ import SwiftUI
 
 
 struct LandmarkDetail: View {
+    @Environment(ModelData.self) var modelData
     var landmark: Landmark
     
+    var landmarkIndex: Int {
+        modelData.landmarks.firstIndex(where: { $0.id == landmark.id})!
+    }
+    
     var body: some View {
+        @Bindable var modelData = modelData
+        
         ScrollView {
             MapView(coordinate: landmark.locationCoordinate)
                 .frame(height: 300)
@@ -21,10 +28,12 @@ struct LandmarkDetail: View {
                 .padding(.bottom, -130)
             
             VStack(alignment: .leading) {
+                HStack {
+                    Text(landmark.name)
+                        .font(.title)
+                    FavoriteButton(isSet: $modelData.landmarks[landmarkIndex].isFavorite)
+                }
                 
-                Text(landmark.name)
-                    .font(.title)
-                    .foregroundColor(.black)
                 HStack {
                     Text(landmark.park)
                     Spacer()
@@ -49,5 +58,7 @@ struct LandmarkDetail: View {
 
 
 #Preview {
-    LandmarkDetail(landmark: ModelData().landmarks[0])
+    let modelData = ModelData()
+    return LandmarkDetail(landmark: modelData.landmarks[0])
+            .environment(modelData)
 }
