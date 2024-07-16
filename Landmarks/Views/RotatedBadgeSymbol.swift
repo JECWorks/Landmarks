beb635a31f0631ed94114b0714bdca57b9ced9e1
JECWorks <36215363+JECWorks@users.noreply.github.com>
2024-07-15 22:10:40 -0400

    Draw the badge symbol

---
+++ Landmarks/Views/RotatedBadgeSymbol.swift
@@ -0,0 +1,22 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmarks
+//
+//  Created by Jason Cox on 7/15/24.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+#Preview {
+    RotatedBadgeSymbol(angle: Angle(degrees: 15))
+}
