e6bae5e5ccb54b216c6cd09333fc0abc2cc9cb08
JECWorks <36215363+JECWorks@users.noreply.github.com>
2024-09-30 22:14:04 -0400

    Add a watchOS target

---
+++ WatchLandmarks Watch App/WatchLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchLandmarksApp.swift
+//  WatchLandmarks Watch App
+//
+//  Created by Jason Cox on 9/30/24.
+//
+
+import SwiftUI
+
+@main
+struct WatchLandmarks_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
